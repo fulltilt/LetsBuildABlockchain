dbebdf278e91891a156ece5109dba3e5089961fb
David Doria <david.doria.ext@nokia.com>
2018-01-30 10:25:26 -0800

    Finished stage 1

--- server.js
+++ server.js
@@ -11,19 +11,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // Configuring the database
-var dbConfig = require('./config/database.config.js');
-var mongoose = require('mongoose');
+// var dbConfig = require('./config/database.config.js');
+// var mongoose = require('mongoose');
 
-mongoose.connect(dbConfig.url);
+// mongoose.connect(dbConfig.url);
 
-mongoose.connection.on('error', function() {
-    console.log('Could not connect to the database. Exiting now...');
-    process.exit();
-});
+// mongoose.connection.on('error', function() {
+//     console.log('Could not connect to the database. Exiting now...');
+//     process.exit();
+// });
 
-mongoose.connection.once('open', function() {
-    console.log("Successfully connected to the database");
-});
+// mongoose.connection.once('open', function() {
+//     console.log("Successfully connected to the database");
+// });
 
 // define a simple route
 app.get('/', function(req, res){
@@ -32,6 +32,10 @@ app.get('/', function(req, res){
 
 // Require Notes routes
 require('./app/routes/note.routes.js')(app);
+
+
+require('./app/haseeb.js')(app);
+
 
 // listen for requests
 app.listen(3000, function(){
